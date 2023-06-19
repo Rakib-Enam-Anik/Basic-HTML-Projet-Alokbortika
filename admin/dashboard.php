f01cc6e30bfc5756e0dc1c7261c3ce842a5499b1
Rakib Enam <md.rakib.enam.anik@bracu.ac.bd>
2023-06-19 14:29:21 +0600

    19 th june Hatsodai file added

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -31,5 +31,104 @@ if(!isset($admin_id)){
 
 <?php include '../components/admin_header.php'?>
 
+<!-- admin dashboard section starts -->
+<section class="dashboard">
+
+<h1 class="heading">dashboard</h1>
+
+  <div class="box-container">
+    <div class="box">
+        <p><?= $fetch_profile['name']; ?></p>
+        <a href="update_profile.php" class="btn">update profile</a>
+</div>
+
+<div class="box">
+    <?php
+    $total_pendings = 0;
+    $select_pendings = $conn->prepare("SELECT * FROM 'orders' WHERE 
+    payment_status =?");
+    $select_pendings->execute(['pending']);
+    while($fetch_pendings = $select_pendings->fetch(PDO::FETCH_ASSOC)){
+        $total_pendings += $fetch_pendings['total_price'];
+    }
+    ?>
+    <h3><span>$</span><?= $total_pendings; ?><span>/-</span></h3>
+    <p>total pendings</p>
+    <a href="placed_orders.php" class="btn">see orders</a>
+</div>
+
+    <div class="box">
+    <?php
+    $total_completes = 0;
+    $select_completes = $conn->prepare("SELECT * FROM 'orders' WHERE 
+    payment_status =?");
+    $select_pendings->execute(['completed']);
+    while($fetch_completes = $select_completes->fetch(PDO::FETCH_ASSOC)){
+        $total_completes += $fetch_completes['total_price'];
+    }
+    ?>
+    <h3><span>$</span><?= $total_completes; ?><span>/-</span></h3>
+    <p>total completes</p>
+    <a href="placed_orders.php" class="btn">see orders</a>
+</div>
+
+<div class="box">
+    <?php
+    $select_orders = $conn->prepare("SELECT * FROM 'orders'");
+    $select_orders->execute();
+    $numbers_of_orders = $select_orders->rowCount();
+    ?>
+    <h3><?= $numbers_of_orders; ?></h3>
+    <p>total orders</p>
+    <a href="placed_orders.php" class="btn">see orders</a>
+</div>
+
+<div class="box">
+    <?php
+    $select_products = $conn->prepare("SELECT * FROM 'products'");
+    $select_products->execute();
+    $numbers_of_products = $select_productss->rowCount();
+    ?>
+    <h3><?= $numbers_of_products; ?></h3>
+    <p>total productss</p>
+    <a href="products.php" class="btn">see products</a>
+</div>
+<div class="box">
+    <?php
+    $select_userss = $conn->prepare("SELECT * FROM 'users'");
+    $select_users->execute();
+    $numbers_of_ussers = $select_users->rowCount();
+    ?>
+    <h3><?= $numbers_of_users; ?></h3>
+    <p> users accounts </p>
+    <a href="users_accounts.php" class="btn">see users</a>
+</div>
+
+
+
+<div class="box">
+    <?php
+    $select_admins = $conn->prepare("SELECT * FROM 'admins'");
+    $select_admins->execute();
+    $numbers_of_admins = $select_admins->rowCount();
+    ?>
+    <h3><?= $numbers_of_admins; ?></h3>
+    <p>admins</p>
+    <a href="admin_accounts.php" class="btn">see admins</a>
+</div>
+
+<div class="box">
+    <?php
+    $select_messages = $conn->prepare("SELECT * FROM 'messages'");
+    $select_messages->execute();
+    $numbers_of_messages = $select_messages->rowCount();
+    ?>
+    <h3><?= $numbers_of_messages; ?></h3>
+    <p>total messages</p>
+    <a href="messages.php" class="btn">see messages</a>
+</div>
+</div>
+</section>
+
 <!-- custom js file link -->
 <script src="../js/admin_script.js"></script>

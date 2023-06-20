2677b44ade7f7cf1d2f44b07c8ba5618269da5d7
Rakib Enam <md.rakib.enam.anik@bracu.ac.bd>
2023-06-20 11:53:04 +0600

    20/6/23  Hatsodai file added

---
+++ admin/register_admin.php
@@ -0,0 +1,32 @@
+<?php 
+include '../components/connect.php';
+
+session_start();
+
+$admin_id = $_SESSION['admin_id'];
+
+if(!isset($admin_id)){
+    header('location:admin_login.php');
+}
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>register</title>
+
+    <!-- font awesome cdn link -->
+    <link rel="stylesheet" href="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/
+    E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==">
+
+
+    <!-- custom css file link -->
+    <link rel="stylesheet" href="../css/admin_style.css">
+</head>
+<body>
+
+<?php include '../components/admin_header.php'?>
---
+++ js/admin_script.js
@@ -0,0 +1,20 @@
+let profile = document.querySelector('.header .flex .profile');
+
+document.querySelector('#user-btn').onclick = () =>{
+    profile.classList.toggle('active');
+    profile.classList.remove('active');
+    navbar.classList.remove('active');
+}
+
+let navbar = document.querySselector('.header .flex .navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.toggle('active');
+    profile.classList.remove('active');
+    navbar.classList.remove('active');
+}
+
+window.onscroll = ()=>{
+    profile.classList.remove('active');
+    navbar.classList.remove('active');
+}

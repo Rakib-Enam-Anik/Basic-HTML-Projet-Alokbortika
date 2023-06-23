983c63cdc366356bf478dd5535185a8e7c26cfcc
Rakib Enam <md.rakib.enam.anik@bracu.ac.bd>
2023-06-23 12:21:35 +0600

    HatSodai 23/6/23 file added

--- admin/products.php
+++ admin/products.php
@@ -30,3 +30,51 @@ if(!isset($admin_id)){
 <body>
 
 <?php include '../components/admin_header.php'?>
+
+<!-- add products section starts -->
+<section class="add-products">
+
+   <h1 class="heading">add product</h1>
+
+   <form action="" method="POST" encytype="multipart/form-data">
+  
+        <div class="flex">
+            <div class="inputBox">
+                <span>product name (required) </span>
+                <input type="text" required placeholder="enter product name"
+                name="name" maxlength="100" class="box">
+            </div>
+
+            <div class="inputBox">
+                <span>product price (required) </span>
+                <input type="number" min="0" max="9999999999" required placeholder="enter product price"
+                name="price" onkeypress="if(this.value.length == 10) return false;" class="box">
+            </div>
+            <div class="inputBox">
+                <span>image 01 (required) </span>
+                <input type="file" name="image_01" class="box" accept="image/jpg,
+                 image/jpeg, image/png, image/webp" required>
+            </div>
+            <div class="inputBox">
+                <span>image 02 (required) </span>
+                <input type="file" name="image_01" class="box" accept="image/jpg,
+                 image/jpeg, image/png, image/webp" required>
+            </div>
+            <div class="inputBox">
+                <span>image 03 (required) </span>
+                <input type="file" name="image_01" class="box" accept="image/jpg,
+                 image/jpeg, image/png, image/webp" required>
+            </div>
+            <div class="inputBox">
+                <span>product details</span>
+                <textarea name="details" class="box"placeholder="enter product details" required 
+                maxlength="500" cols="30" rows="10"></textarea>
+            </div>
+            <input type="submit" value="add product" name="add_product" class="btn">
+</div>
+</form>
+</section>  
+       
+
+
+<!-- add products section ends -->
--- admin/update_profile.php
+++ admin/update_profile.php
@@ -7,6 +7,83 @@ $admin_id = $_SESSION['admin_id'];
 
 if(!isset($admin_id)){
     header('location:admin_login.php');
+};
+
+if(isset($_POST['add_product'])){
+    $name = $_POST['name'];
+    $name = filter_var($name, FILTER_SANITIZE_STRING);
+    $price = $_POST['price'];
+    $price = filter_var($price, FILTER_SANITIZE_STRING);
+    $details = $_POST['details'];
+    $details = filter_var($details, FILTER_SANITIZE_STRING);
+
+    $image_01 = $_FILES['image_01']['name'];
+    $image_01 = filter_var($image_01, FILTER_SANITIZE_STRING);
+    $image_01_size = $_FILES['image_01']['size'];
+    $image_01_tmp_name = $_FILES['image_01']['tmp_name'];
+    $image_01_folder = '../upload_img/'.$image_01;
+
+    $image_02 = $_FILES['image_02']['name'];
+    $image_02 = filter_var($image_02, FILTER_SANITIZE_STRING);
+    $image_02_size = $_FILES['image_02']['size'];
+    $image_02_tmp_name = $_FILES['image_02']['tmp_name'];
+    $image_02_folder = '../upload_img/'.$image_02;
+
+    $image_03 = $_FILES['image_03']['name'];
+    $image_03 = filter_var($image_03, FILTER_SANITIZE_STRING);
+    $image_03_size = $_FILES['image_03']['size'];
+    $image_03_tmp_name = $_FILES['image_03']['tmp_name'];
+    $image_03_folder = '../upload_img/'.$image_03;
+
+    $select_products = $conn->prepare("SELECT * FROM 'products' WHERE name = ? ");
+    $select_products->execute([$name]);
+
+    if($select_products->rowCount() > 0){
+        $message[] = 'product name already exists!';
+    }else{
+        $insert_product = $conn->prepare("INSERT INTO 'products'(name, details, price, image_01, image_02, image_03) ")
+        VALUES(?,?,?,?));
+    }
+}
+
+if(isset($_POST['submit'])){
+
+    $name = $_POST['name'];
+    $name = filter_var($name, FILTER_SANITIZE_STRING);
+
+    $update_name = $conn->prepare("UPDATE 'admins' SET name = ? WHERE id = ?");
+    $update_name-> execute([$name, $admin_id]);
+
+    $empty_pass = '';
+    $select_old_pass = $conn->prepare("SELECT password FROM `admins` WHERE id = ?");
+    $select_old_pass->execute([$admin_id]);
+    $fetch_prev_pass = $select_old_pass->fetch(PDO::FETCH_ASSOC);
+    $prev_pass = $fetch_prev_pass['password'];
+    $old_pass = sha1($_POST['old_pass']);
+    $old_pass = filter_var($old_pass, FILTER_SANITIZE_STRING);
+    $new_pass = sha1($_POST['new_pass']);
+    $new_pass = filter_var($new_pass, FILTER_SANITIZE_STRING);
+    $confirm_pass = sha1($_POST['confirm_pass']);
+    $confirm_pass = filter_var($confirm_pass, FILTER_SANITIZE_STRING);  
+
+    if($old_pass == $empty_pass){
+        $message[] = 'please enter old password!';
+    }elseif($old_pass != $new_pass){
+        $message[] = 'old password not matched!';
+    }elseif($new_pass != $confirm_pass){
+        $message[] = 'confirm password not matched! ';
+    }else{
+        if($new_pass != $empty_pass){
+            $update_pass = $conn->preppare("UPDATE 'admins' SET password = ?
+            WHERE id = ?");
+            $update_pass->execute([$confirm_pass, $admin_id]);
+            $message[] = 'password update successfully!';
+        }else{
+            $message[] = 'please enter the new password!';
+        }
+    }
+
+}
 }
 
 

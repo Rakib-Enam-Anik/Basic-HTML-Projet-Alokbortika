1a126e762a1bbfae6b7b4f4eca76a29a21e3a2db
Rakib Enam <md.rakib.enam.anik@bracu.ac.bd>
2023-07-03 12:41:51 +0600

    3/7/23 hatsodai file added

--- admin/products.php
+++ admin/products.php
@@ -9,6 +9,23 @@ if(!isset($admin_id)){
     header('location:admin_login.php');
 }
 
+if(isset($_GET['delete'])){
+    $delete_id = $_GET['delete'];
+    $delete_product_image = $conn->prepare("SELECT * FROM 'products' WHERE id = ?");
+    $delete_product_image->execute([$delete_id]);
+    $fetch_delete_image = $delete_product_image->fetch(PDO::FETCH_ASSOC);
+    $fetch_delete_image = $delete_product_image->fetch(PDO::FETCH_ASSOC);
+    unlink('../upload_img/'.$fetch_delete_image['image_01']);
+    unlink('../upload_img/'.$fetch_delete_image['image_02']);
+    unlink('../upload_img/'.$fetch_delete_image['image_03']);
+    $delete_product = $conn->prepare("DELETE FROM 'products' WHERE id = ?");
+    $delete_product->execute([$delete_id]);
+    $delete_product = $conn->prepare("DELETE FROM 'cart' WHERE pid = ?");
+    $delete_product->execute([$delete_id]);
+    $delete_product = $conn->prepare("DELETE FROM 'wishlist' WHERE pid = ?");
+    $delete_product->execute([$delete_id]);
+    header('location:products.php');
+}
 
 ?>
 
@@ -90,6 +107,23 @@ if(!isset($admin_id)){
 $show_product = $conn->prepare("SELECT * FROM 'products'");
  $show_products->execute();
  if($show_products>rowCount() > 0){
+    while($fetch_products = $show_product->fetch(PDO::FETCH_ASSOC)){
+        ?>
+    <div class="box">
+        <img src="../uploaded_img/<?= $fetch_pproducts['image_01']; ?>" alt="">
+        <div class="name"><?= $fetch_products['name']; ?></div>
+        <div class="price"><?= $fetch_products['price']; ?></div>
+        <div class="details"><?= $fetch_products['details']; ?></div>
+        <div class="flex-btn">
+            <a href="update_product.php?update=<?= $fetch_products['id']; ?>"
+            class="option-btn">update</a>
+            <a href="update_product.php?update=<?= $fetch_products['id']; ?>"
+            class="option-btn" onclick="return confirm('delete this product?');">delete</a>
+    </div>
+    </div>
+
+
+    }
 
  }else{
     echo '<p class="empty"> no products added yet!</p>';
